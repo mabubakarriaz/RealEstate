83a1d98308278649a9f8557f1715586e16428035
mabubakarriaz <abubakar.riaz@outlook.com>
2016-06-05 15:07:04 +0500

    Console Project

---
+++ Real Estate Console App/Real Estate Console App/Program.cs
@@ -0,0 +1,53 @@
+﻿using Com.RealEstate.Component.Data;
+using Com.RealEstate.Component.DataHelper;
+using Com.RealEstate.Component.Entity;
+using Com.RealEstate.Component.Handler;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_Estate_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ProductTransaction aProductTransaction = new ProductTransaction();
+            //aProductTransaction.DealID = 1;
+            //aProductTransaction.TransactionType = ProductTransactionType.Sale;
+            //aProductTransaction.TransactionAmount = 40000;
+
+            SqlConnection con = new ConnectionString().GetConnection();
+
+            con.Open();
+
+            using (con)
+            {
+                SqlTransaction tran = con.BeginTransaction();
+
+                try
+                {
+                    //new DealDataHelper().UpdateByTransactionAmount(1, 45050, "Sale");
+                    new DealHandler().ChangeByRemainingAmount(2, 90000, "Sale", tran);
+
+                    tran.Commit();
+                }
+                catch (Exception ex)
+                {
+                    tran.Rollback();
+                    throw ex;
+                }
+
+            }
+            
+
+                Console.WriteLine("Program ended...");
+            Console.ReadKey();
+        }
+
+        
+    }
+}
